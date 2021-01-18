2aa22798f40a7e2c33b17bc3ac164b2ca3e16624
Kino  Rose <kinoarose@gmail.com>
2021-01-18 07:08:44 -0500

    Add Font Sizes across the board to remove putting the text size across the application

--- source/Globals.hx
+++ source/Globals.hx
@@ -8,6 +8,7 @@ class Globals {
 	public static inline var TEXT_GAME_OVER = 'Game Over';
 	public static inline var TEXT_OPTIONS = 'Options';
 	public static inline var TEXT_RETRY = 'Retry';
+	public static inline var TEXT_STATS = 'Stats';
 	public static inline var TEXT_TO_TITLE = 'To Title';
 	public static inline var TEXT_BACK = 'Back';
 	public static inline var TEXT_LEVEL_NEXT = 'Next';
@@ -16,7 +17,30 @@ class Globals {
 	public static inline var TEXT_VERSION = 'Version: 0.0.1';
 	public static inline var TIME_BONUS:Int = 100;
 	public static inline var CMD_TIME_SCALE = 0.015;
+	// Player Globals
 	public static inline var PLAYER_HEALTH_CAP = 3;
 	public static inline var PLAYER_BULLET_CD:Float = 0.15;
+	public static inline var PLAYER_GAL_NAME:String = 'Koyuki';
 	public static var HIGH_SCORE:Int = 0;
+	// Font Sizes
+
+	/**
+	 *  Header Font
+	 */
+	public static inline var FONT_H:Int = 32;
+
+	/**
+	 * Sub Header
+	 */
+	public static inline var FONT_SUB_H:Int = 24;
+
+	/**
+	 * Large Font Size
+	 */
+	public static inline var FONT_L:Int = 16;
+
+	/**
+	 * Normal Size
+	 */
+	public static inline var FONT_N:Int = 12;
 }
--- source/game/states/StatsSubState.hx
+++ source/game/states/StatsSubState.hx
@@ -1,14 +1,20 @@
 package game.states;
 
+import flixel.util.FlxAxes;
+
 class StatsSubState extends FlxSubState {
 	public var background:FlxSprite;
-	public var charSprite:FlxSprite;
+	public var titleText:FlxText;
+	public var player:FlxSprite;
 
 	override public function create() {
 		FlxG.mouse.visible = true;
 		createBackground();
+		createTitle();
 		createButtons();
+		createStatSheet();
 		createCharacter();
+		super.create();
 	}
 
 	public function createBackground() {
@@ -21,6 +27,15 @@ class StatsSubState extends FlxSubState {
 			KColor.TRANSPARENT); // Draw Right
 		background.drawRect(leftPortion, 0, rightPortion, FlxG.height,
 			KColor.BURGUNDY);
+		add(background);
+	}
+
+	public function createTitle() {
+		titleText = new FlxText(0, 0, -1, Globals.TEXT_STATS, Globals.FONT_H);
+		titleText.screenCenter(FlxAxes.X);
+		titleText.y += 20;
+		titleText.scrollFactor.set(0, 0);
+		add(titleText);
 	}
 
 	public function createButtons() {
@@ -28,11 +43,37 @@ class StatsSubState extends FlxSubState {
 		add(exitButton);
 	}
 
+	public function createStatSheet() {
+		var padding = 24;
+		var textSize = Globals.FONT_L;
+		var lineHeight = 12 + textSize;
+		var leftPortion = (FlxG.width / 3) * 2;
+		var rightPortion = FlxG.width / 3;
+		var x = leftPortion + padding;
+		var y = 100;
+
+		var nameText = new FlxText(x, y, -1, Globals.PLAYER_GAL_NAME,
+			cast textSize * 1.5);
+		add(nameText);
+		y += lineHeight + 12;
+		background.drawRect(x, y, rightPortion - padding, 4);
+
+		y += lineHeight;
+		// Draw Stats
+		var hpText = new FlxText(x, y, -1, 'HP:', textSize);
+		add(hpText);
+		y += lineHeight;
+		var defText = new FlxText(x, y, -1, 'Def:', textSize);
+		add(defText);
+		y += lineHeight;
+	}
+
 	public function createCharacter() {
 		var leftPortion = (FlxG.width / 3) * 2;
 		var position = leftPortion / 2;
-		charSprite = new FlxSprite(position, FlxG.height / 2);
-		charSprite.makeGraphic(32, 32, FlxColor.WHITE);
+		player = new FlxSprite(position, FlxG.height / 2);
+		player.makeGraphic(32, 32, FlxColor.WHITE);
+		add(player);
 	}
 
 	override public function update(elapsed:Float) {
