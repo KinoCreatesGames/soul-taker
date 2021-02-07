433fcd906508b18e88c0c4ece19ca26d249170d3
kinoar <kinarose@gmail.com>
2021-02-07 01:39:42 +0000

    Update game website

--- docs/bin/haxe-flixel-template.js
+++ docs/bin/haxe-flixel-template.js
@@ -869,7 +869,7 @@ ApplicationMain.main = function() {
 ApplicationMain.create = function(config) {
 	var app = new openfl_display_Application();
 	ManifestResources.init(config);
-	app.meta.h["build"] = "41";
+	app.meta.h["build"] = "42";
 	app.meta.h["company"] = "KinoCreatesGames";
 	app.meta.h["file"] = "haxe-flixel-template";
 	app.meta.h["name"] = "Soul taker";
@@ -67283,7 +67283,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 541988;
+	this.version = 490770;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = "lime.utils.AssetCache";
